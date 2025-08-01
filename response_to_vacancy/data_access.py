06b92b80cc3cec095e133f9948a9a6e4b8c5fcec
Hagai <>
2025-08-01 13:45:18 +0500

    refactor: move report_vacancy() from access to report tools

--- response_to_vacancy/data_access.py
+++ response_to_vacancy/data_access.py
@@ -1,6 +1,4 @@
-from enum import StrEnum, auto
-from itertools import chain
-from json import dump, dumps, load
+from json import dump, load
 from operator import itemgetter
 from pathlib import Path
 from typing import Any
@@ -37,14 +35,12 @@ def get_vacancy_meta_data() -> str:
         if sr_t := sr["to"]:
             rep.append(f"до {sr_t} {sr_cr}")
         rep.append(f"{sr["mode"]["name"].lower()}.")
-    if (
-        (wf := data["work_format"])
-        or (wh := data["working_hours"])
-        or (wsbd := data["work_schedule_by_days"])
-    ):
-        rep.append(
-            f"Возможно работать {", ".join(map(itemgetter("name"), chain(wf, wh, wsbd)))}."
-        )
+    if wf := [
+        *data["work_format"],
+        *data["working_hours"],
+        *data["work_schedule_by_days"],
+    ]:
+        rep.append(f"Возможно работать {", ".join(map(itemgetter("name"), wf))}.")
     rep.append(f"{data["employment_form"]["name"]} занятость.")
     return " ".join(rep)
 
@@ -79,56 +75,3 @@ def get_extended_employer_meta_data() -> str:
     rep = []
     rep.append(get_employer_meta_data())
     return " ".join(rep)
-
-
-class Reliability(StrEnum):
-    ESTABLISHED_AND_PURPOSEFUL = auto()
-    STABLE_BUT_DIRECTIONLESS = auto()
-    YOUNG_AND_CONSTRUCTIVELY_ALIGNED = auto()
-    UNFOCUSED_OR_INCONSISTENT = auto()
-    OVERCONFIDENT_WITH_UNCLEAR_GROUND = auto()
-    OPERATIONALLY_ERRATIC = auto()
-    OPAQUE_AND_REACTIVE = auto()
-    TRANSIENT_OR_SUPERFICIAL = auto()
-
-
-def report_vacancy(
-    is_remote: bool,
-    location: str,
-    reliability: Reliability,
-    skills_match: float,
-    work_tasks: list[str],
-    chances_of_offer: float,
-    advices: list[str],
-) -> None:
-    """Show job report vacancy.
-
-    Collect analysis data from AI agent and show job report vacancy for
-    user reading.
-
-    Args:
-        is_remote (bool): True if offer remote work, otherwise False
-        location (str): location of work office
-        reliability (Reliability): employer reliability
-        skills_match (float): AI assessment of the match between user and
-            requirements skills
-        work_tasks (list[str]): tasks to do at work
-        chances_of_offer (float): AI assessment of chances of getting
-            an offer
-        advices (list[str]): AI advices to increase chances of getting
-            an offer
-    """
-    print(
-        dumps(
-            {
-                "is_remote": is_remote,
-                "location": location,
-                "reliability": reliability,
-                "skills_match": skills_match,
-                "work_tasks": work_tasks,
-                "chances_of_offer": chances_of_offer,
-                "advices": advices,
-                "vacancy": get_vacancy_raw_data(),
-            }
-        )
-    )
--- response_to_vacancy/report_templates.py
+++ response_to_vacancy/report_templates.py
@@ -1,6 +1,65 @@
+from enum import StrEnum, auto
+from json import dumps
+
+from .data_access import get_vacancy_raw_data
+
+
 def prepare_cover_letter():
     pass
 
 
 def make_response():
     pass
+
+
+class Reliability(StrEnum):
+    ESTABLISHED_AND_PURPOSEFUL = auto()
+    STABLE_BUT_DIRECTIONLESS = auto()
+    YOUNG_AND_CONSTRUCTIVELY_ALIGNED = auto()
+    UNFOCUSED_OR_INCONSISTENT = auto()
+    OVERCONFIDENT_WITH_UNCLEAR_GROUND = auto()
+    OPERATIONALLY_ERRATIC = auto()
+    OPAQUE_AND_REACTIVE = auto()
+    TRANSIENT_OR_SUPERFICIAL = auto()
+
+
+def report_vacancy(
+    is_remote: bool,
+    location: str,
+    reliability: Reliability,
+    skills_match: float,
+    work_tasks: list[str],
+    chances_of_offer: float,
+    advices: list[str],
+) -> None:
+    """Show job report vacancy.
+
+    Collect analysis data from AI agent and show job report vacancy for
+    user reading.
+
+    Args:
+        is_remote (bool): True if offer remote work, otherwise False
+        location (str): location of work office
+        reliability (Reliability): employer reliability
+        skills_match (float): AI assessment of the match between user and
+            requirements skills
+        work_tasks (list[str]): tasks to do at work
+        chances_of_offer (float): AI assessment of chances of getting
+            an offer
+        advices (list[str]): AI advices to increase chances of getting
+            an offer
+    """
+    print(
+        dumps(
+            {
+                "is_remote": is_remote,
+                "location": location,
+                "reliability": reliability,
+                "skills_match": skills_match,
+                "work_tasks": work_tasks,
+                "chances_of_offer": chances_of_offer,
+                "advices": advices,
+                "vacancy": get_vacancy_raw_data(),
+            }
+        )
+    )
