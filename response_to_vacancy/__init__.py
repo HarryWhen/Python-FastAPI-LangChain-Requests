e1e0e199d8edf47831a1bcf718583d5c9faa3f45
Hagai <>
2025-08-04 15:56:40 +0500

    refactor: rename REPL methods

--- response_to_vacancy/__init__.py
+++ response_to_vacancy/__init__.py
@@ -8,15 +8,15 @@ class Message:
     text: str
 
 
-def get_prompt(*, chat: simple_chat.AnyChat) -> Message:
+def get_message(*, chat: simple_chat.AnyChat) -> Message:
     return Message(chat.read())
 
 
-def respond_to(msg: Message, *, agent) -> Message:
+def process(msg: Message, *, agent) -> Message:
     return Message("Даже я не могу разобрать, что ты хочешь")
 
 
-def print_msg(msg: Message, *, chat: simple_chat.AnyChat) -> None:
+def reply_with(msg: Message, *, chat: simple_chat.AnyChat) -> None:
     chat.print(msg.text)
 
 
@@ -25,6 +25,6 @@ def run():
     agent = ai_agent.get_agent()
 
     while True:
-        prompt = get_prompt(chat=chat)
-        response = respond_to(prompt, agent=agent)
-        print_msg(response, chat=chat)
+        prompt = get_message(chat=chat)
+        response = process(prompt, agent=agent)
+        reply_with(response, chat=chat)
