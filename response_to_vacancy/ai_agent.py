20ee3027de56b8d2562ef2c8c9bff024e56889df
Hagai <>
2025-08-04 17:42:34 +0500

    feat: implement LLMAgent to respond to user message

--- response_to_vacancy/ai_agent.py
+++ response_to_vacancy/ai_agent.py
@@ -1,17 +1,42 @@
 from dataclasses import dataclass
 
+from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
+from langgraph.graph.state import CompiledStateGraph
+from langgraph.prebuilt import create_react_agent
 
 from ._common import Message
 
 
+def get_prompt() -> str:
+    return ""
+
+
 class LLMAgent:
-    def __init__(self):
-        pass
+    def __init__(self, agent: CompiledStateGraph) -> None:
+        self._agent = agent
 
     def respond_to(self, msg: Message) -> Message:
-        return Message("Даже я не могу разобрать, что ты хочешь")
+        ai_output = self._agent.invoke(
+            {
+                "messages": [
+                    HumanMessage(msg.text),
+                ],
+            }
+        )
+        return Message(ai_output["messages"][-1].content)
 
 
 def get_agent() -> LLMAgent:
-    return LLMAgent()
+    llm = ChatOllama(
+        model="llama3.2",
+        temperature=0,
+    )
+
+    agent = create_react_agent(
+        model=llm,
+        tools=[],
+        prompt=get_prompt(),
+    )
+
+    return LLMAgent(agent)
